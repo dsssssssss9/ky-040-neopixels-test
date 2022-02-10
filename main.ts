e958c0ff1edd2608553647b453c43fe5734b8547
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-10 22:05:04 +0000

    pixel 9 light green?

--- main.ts
+++ main.ts
@@ -1,16 +1,24 @@
 KY040.OnPinPressed(DigitalPin.P14, function () {
-    basic.showIcon(IconNames.Heart)
+    Pix_Numb = 0
+    strip.clear()
+    strip.show()
+    basic.showIcon(IconNames.Tortoise)
 })
 KY040.onTurned(direction.clockwise, function () {
     basic.showArrow(ArrowNames.West)
 })
 KY040.onTurned(direction.counterclockwise, function () {
     basic.showArrow(ArrowNames.East)
+    Pix_Numb += 1
 })
+let Pix_Numb = 0
+let strip: neopixel.Strip = null
 KY040.setKY040(DigitalPin.P15, DigitalPin.P1)
-let strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+Pix_Numb = 0
+strip.clear()
 basic.showIcon(IconNames.Heart)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
 basic.forever(function () {
-	
+    strip.setPixelColor(Pix_Numb, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
 })
